659f41ffc08dd6b794e0ee5d5402edf152d1d8c9
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-11 14:31:40 -0300

    add: new vim-fugitive keymaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,6 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git);                 -- fugitive interface with status and interative staging
+vim.keymap.set("n", "<leader>ga", ":G add ");                   -- opens git add command ready for parameters 
+vim.keymap.set("n", "<leader>gc", ":G commit -m \"\"<left>");   -- opens git commit with the cursor between quotes
+vim.keymap.set("n", "<leader>gp", ":G push");                   -- no <CR> at the end to prevent accidents
+
 
