a5f0ee8c400ed0f1ca3b926d851474b621166f74
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-31 19:45:37 -0300

    add: line wrap toggle keybinding

--- lua/cfg/remap.lua
+++ lua/cfg/remap.lua
@@ -58,6 +58,9 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- Replace every instance of word at cursor
 vim.keymap.set("n", "<leader>rn", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
+-- Sometimes is useful to have line wrap on
+vim.keymap.set("n", "<leader>tlw", function() vim.wo.wrap = not vim.wo.wrap end)
+
 -- For making Bash scrpts executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
