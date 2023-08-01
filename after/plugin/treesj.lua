c89fa51071dd12843bd388c473545fe71b27c115
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-01 18:50:22 -0300

    add: TreeSJ keybindings

---
+++ after/plugin/treesj.lua
@@ -0,0 +1,9 @@
+
+-- toogle sj
+vim.keymap.set("n", "<leader>tsj", vim.cmd.TSJToggle)
+
+-- sj split
+vim.keymap.set("n", "<leader>sjs", vim.cmd.TSJSplit)
+
+-- join sj
+vim.keymap.set("n", "<leader>jsj", vim.cmd.TSJJoin)
