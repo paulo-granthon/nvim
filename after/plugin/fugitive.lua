7c0612a41547aa66647db6deb1a7c727bc7343e6
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-10 09:49:56 -0300

    chore: add space at the end of `push` fugitive keybinding

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -9,7 +9,7 @@ vim.keymap.set('n', '<leader>gf', ":G fetch<CR>")               -- fetch
 vim.keymap.set('n', '<leader>gj', ":G pull<CR>")                -- pull
 vim.keymap.set("n", "<leader>ga", ":G add ")                    -- opens git add command ready for parameters
 vim.keymap.set("n", "<leader>gc", ":G commit -m \"\"<left>")    -- opens git commit with the cursor between quotes
-vim.keymap.set("n", "<leader>gk", ":G push")                    -- no <CR> at the end to prevent accidents
+vim.keymap.set("n", "<leader>gk", ":G push ")                    -- no <CR> at the end to prevent accidents
 vim.keymap.set("n", "<leader>gd", ":G diff<CR>")
 vim.keymap.set("n", "<leader>gl", ":G log<CR>")
 
