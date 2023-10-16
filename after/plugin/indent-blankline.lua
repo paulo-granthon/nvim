de1ddc0f8f16a48789980cc2ebfd36cf614ccd3f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-16 19:03:34 -0300

    fix: `ibl` config

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -5,11 +5,10 @@ vim.opt.listchars:append 'trail: '
 vim.cmd [[highlight IndentBlanklineContextChar guifg=#C678DD gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineContextStart guisp=#C678DD gui=underline]]
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-
-    show_trailing_blankline_indent = true,
-}
+require("ibl").setup({
+    scope = {
+        enabled = true,
+        show_start = true,
+    },
+})
 
