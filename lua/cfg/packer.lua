647729833ba4902659caec1021e63b1bde6cb4dc
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-16 19:03:44 -0300

    chore: bump `telescope` version

--- lua/cfg/packer.lua
+++ lua/cfg/packer.lua
@@ -23,7 +23,7 @@ return require('packer').startup(function(use)
 
     -- faster file navigation
     use{
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
