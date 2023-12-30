494f40f59f46d44670c0391f516bf09dfb8afc18
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-12-29 22:46:12 -0300

    style: fix indentation

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,21 +1,21 @@
 function ColorMyPencils(color)
-	if color then
-		vim.cmd.colorscheme(color)
-	else
-		require('onedark').setup {
-			style = 'darker'
-		}
-		require('onedark').load()
-		color = 'onedark'
-	end
+    if color then
+        vim.cmd.colorscheme(color)
+    else
+        require('onedark').setup {
+            style = 'darker'
+        }
+        require('onedark').load()
+        color = 'onedark'
+    end
 
     -- remove default neovim backgrounds
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
     -- remove gitsigns backgrounds
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-
 end
+
 ColorMyPencils()
