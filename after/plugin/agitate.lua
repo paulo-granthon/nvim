b51d7e4a61bdd1c6f3b3ebc2798faef67c948a66
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 16:32:42 -0300

    docs: add comments in `agitate` configs

--- after/plugin/agitate.lua
+++ after/plugin/agitate.lua
@@ -1,11 +1,16 @@
+-- Load `agitate` module | Return early if error
 local agitate_ok, agitate_or_err = pcall(require, 'agitate')
 if not agitate_ok then return agitate_or_err end
 
+-- Load env for `agitate`
 local env_ok, env_or_err = pcall(require, 'env.agitate')
 
+-- Setup `agitate` with `env` if ok | Error: `{err = <msg>}`
+-- No early return so that keybindings are still set even if no env
 agitate_or_err.setup(env_ok and env_or_err or {
     err = env_or_err
 })
 
+-- Set keybindings
 vim.keymap.set('n', '<leader>gi', ':AgitateRepoInitGitHub')
 vim.keymap.set('n', '<leader>gbp', ':AgitateBranchCreateCheckoutAndPush ')
