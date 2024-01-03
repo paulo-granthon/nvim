66b1f0409cb93cea062b2f18257f79b6526d256a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 16:25:18 -0300

    feat: setup `agitate` with `err` as `opts` if not `env_or_err` instead of returning

--- after/plugin/agitate.lua
+++ after/plugin/agitate.lua
@@ -2,9 +2,10 @@ local agitate_ok, agitate_or_err = pcall(require, 'agitate')
 if not agitate_ok then return agitate_or_err end
 
 local env_ok, env_or_err = pcall(require, 'env.agitate')
-if not env_ok then return env_or_err end
 
-agitate_or_err.setup(env_or_err)
+agitate_or_err.setup(env_ok and env_or_err or {
+    err = env_or_err
+})
 
 vim.keymap.set('n', '<leader>gi', ':AgitateRepoInitGitHub')
 vim.keymap.set('n', '<leader>gbp', ':AgitateBranchCreateCheckoutAndPush ')
