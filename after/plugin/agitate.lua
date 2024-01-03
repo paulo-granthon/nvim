f7e9fc12c55bd45a6615d0d3bae84616c7f42c5f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 16:10:15 -0300

    feat: conditional config for `agitate`

--- after/plugin/agitate.lua
+++ after/plugin/agitate.lua
@@ -1,6 +1,10 @@
-require('agitate').setup({
-    github_username = require('env.agitate').github_username
-})
+local agitate_ok, agitate_or_err = pcall(require, 'agitate')
+if not agitate_ok then return agitate_or_err end
+
+local env_ok, env_or_err = pcall(require, 'env.agitate')
+if not env_ok then return env_or_err end
+
+agitate_or_err.setup(env_or_err)
 
 vim.keymap.set('n', '<leader>gi', ':AgitateRepoInitGitHub')
 vim.keymap.set('n', '<leader>gbp', ':AgitateBranchCreateCheckoutAndPush ')
