1954e2377155a1e3055575c0d1eba279af2c3218
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-03 01:09:46 -0300

    feat: add `agitate` keybindings

--- after/plugin/agitate.lua
+++ after/plugin/agitate.lua
@@ -1,3 +1,6 @@
 require('agitate').setup({
     github_username = require('env.agitate').github_username
 })
+
+vim.keymap.set('n', '<leader>gi', ':AgitateRepoInitGitHub')
+vim.keymap.set('n', '<leader>gbp', ':AgitateBranchCreateCheckoutAndPush ')
