f450e8bfdeb23a2c54530ae0fb330807b571a149
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-08 00:43:51 -0300

    style: use `'` instead of `"` in `plugin.colors`

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -19,12 +19,12 @@ function ColorMyPencils(color)
         vim.api.nvim_err_writeln(theme_or_err)
     end
 
-    local no_bg = { bg = "none" }
+    local no_bg = { bg = 'none' }
 
     -- remove backgrounds
-    vim.api.nvim_set_hl(0, "Normal", no_bg)
-    vim.api.nvim_set_hl(0, "NormalFloat", no_bg)
-    vim.api.nvim_set_hl(0, "EndOfBuffer", no_bg)
+    vim.api.nvim_set_hl(0, 'Normal', no_bg)
+    vim.api.nvim_set_hl(0, 'NormalFloat', no_bg)
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', no_bg)
 
     vim.api.nvim_set_hl(0, 'TabLineFill', no_bg)
     vim.api.nvim_set_hl(0, 'TabLine', no_bg)
@@ -34,17 +34,17 @@ function ColorMyPencils(color)
     vim.api.nvim_set_hl(0, 'NonText', no_bg)
 
     local line_nr_colors = {
-        bg = "none",
-        fg = "gray"
+        bg = 'none',
+        fg = 'gray'
     }
 
     -- remove line number column backgrounds
     vim.api.nvim_set_hl(0, 'LineNrAbove', line_nr_colors)
-    vim.api.nvim_set_hl(0, 'LineNr', { fg = "white", bg = "none" })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bg = 'none' })
     vim.api.nvim_set_hl(0, 'LineNrBelow', line_nr_colors)
 
     -- remove gitsigns backgrounds
-    vim.api.nvim_set_hl(0, "SignColumn", no_bg)
+    vim.api.nvim_set_hl(0, 'SignColumn', no_bg)
 end
 
 ColorMyPencils()
