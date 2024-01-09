648cced06c37f0abe18b4912050156ce7eaf0a87
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-09 14:14:53 -0300

    feat: add `trouble` plugin config

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,1 +1,12 @@
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle workspace_diagnostics<cr>", {silent = true, noremap = true})
+require('trouble').setup {
+    height = 16,
+    padding = false,
+    action_keys = {
+        toggle_fold = { "zA", "za", "=" },
+    }
+}
+
+vim.keymap.set(
+    "n", "<leader>xq", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+    { silent = true, noremap = true }
+)
