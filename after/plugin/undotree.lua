5088b4d1bd512f857b8656c613ebb534a3fffa7e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-10 14:09:20 -0300

    style: apply `lsp` formatting to `plugin.undotree`

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,9 +1,9 @@
 vim.opt.swapfile = false
 vim.opt.backup = false
-if package.config:sub(1,1) == '\\' then
+if package.config:sub(1, 1) == '\\' then
     vim.opt.undodir = os.getenv("LOCALAPPDATA") .. "\\.vim\\undodir" -- Windows
 else
-    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- Linux
+    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"           -- Linux
 end
 
 vim.opt.undofile = true
