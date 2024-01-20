1b098b75c812e6fa27180ff95508866a6dece8d5
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-20 19:53:00 -0300

    feat: update `conform` plugin configs

--- plugin/lsp/formatting.lua
+++ plugin/lsp/formatting.lua
@@ -1,3 +1,33 @@
 return {
   'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    require('conform').setup({
+      formatters_by_ft = {
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        json = { 'prettier' },
+        html = { 'prettier' },
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
+        rust = { 'rustfmt' },
+        go = { 'gofmt' },
+        markdown = { 'prettier' },
+        yaml = { 'prettier' },
+        toml = { 'prettier' },
+        sh = { 'shfmt' },
+        css = { 'prettier' },
+      },
+    })
+
+    vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
+      require('conform').format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { description = 'Format file with Conform.nvim' })
+  end
 }
