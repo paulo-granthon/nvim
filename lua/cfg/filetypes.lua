62f6005755e764033dc60c488c954038a3839bea
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-21 20:23:12 -0300

    style: apply linting to `cfg.filetypes`

--- lua/cfg/filetypes.lua
+++ lua/cfg/filetypes.lua
@@ -1,25 +1,23 @@
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = {
-        'html',
-        'css',
-        'js',
-        'ts',
-        'tsx',
-        'jsx',
-        'json',
-    },
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-    end
+  pattern = {
+    'html',
+    'css',
+    'js',
+    'ts',
+    'tsx',
+    'jsx',
+    'json',
+  },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = {
-        'js',
-        'Jsx',
-    },
-    callback = function()
-        require'lspconfig'.tsserver.setup{}
-    end
+  pattern = {
+    'js',
+    'Jsx',
+  },
+  callback = function() require('lspconfig').tsserver.setup({}) end,
 })
