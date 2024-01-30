40ab8023871abd461a7f44d9df692ac9868dc31c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-30 13:16:46 -0300

    feat: add `oil` config

---
+++ plugin/oil.lua
@@ -0,0 +1,8 @@
+require('oil').setup({
+  skip_confirm_for_simple_edits = true,
+  view_options = {
+    show_hidden = true,
+  },
+})
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
