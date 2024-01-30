99fa1efb7e4b7c8ec70497b246ffefc0aeb2f6a6
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-30 13:17:35 -0300

    fix: fix `oil` compatibility with `VimEnter` event in `cfg.autocmd`

--- lua/cfg/autocmd.lua
+++ lua/cfg/autocmd.lua
@@ -1,13 +1,31 @@
-local open_netrw_if_no_file_arg = function()
-    local _, target_arg = pcall(function()
-        return vim.v.argv[3]
-    end)
+local silent = true
 
-    if (not target_arg or target_arg == '.') then
-        vim.cmd("silent! Explore")
-    end
+local oil_cmd = 'Oil'
+local nrw_cmd = 'Explore'
+
+local wrap_cmd_in_function = function(cmd)
+  local silent_flag = silent and 'silent! ' or ''
+  cmd = silent_flag .. cmd
+  return function() vim.cmd(cmd) end
 end
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-    callback = open_netrw_if_no_file_arg,
+local add_no_file_arg_check_to_function = function(fun_of_choice)
+  return function()
+    local _, target_arg = pcall(function() return vim.v.argv[3] end)
+    if not target_arg or target_arg == '.' then fun_of_choice() end
+  end
+end
+
+local get_no_file_arg_function = function()
+  local oil_ok, _ = pcall(require, 'oil')
+  return add_no_file_arg_check_to_function(wrap_cmd_in_function(oil_ok and oil_cmd or nrw_cmd))
+end
+
+local no_file_arg_callback = vim.schedule_wrap(function()
+  local vim_enter_no_arg_function = get_no_file_arg_function()
+  return vim_enter_no_arg_function()
+end)
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  callback = no_file_arg_callback,
 })
