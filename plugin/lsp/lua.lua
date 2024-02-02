00bd6930143458590f777941c1720b70fcec1d13
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-01 22:33:36 -0300

    chore: add globals to lua lspconfig in `plugin.lsp.lua`

--- plugin/lsp/lua.lua
+++ plugin/lsp/lua.lua
@@ -4,6 +4,13 @@ require('lspconfig').lua_ls.setup({
     if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
+          diagnostics = {
+            globals = {
+              'vim',
+              'lsp',
+              'client_capabilities',
+            },
+          },
           runtime = {
             -- Tell the language server which version of Lua you're using
             -- (most likely LuaJIT in the case of Neovim)
