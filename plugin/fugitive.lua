4d1804e497c5b91aa9dd55454cab042ed77594e9
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-01 22:34:01 -0300

    chore: apply formatting to `plugin.fugitive`

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -1,22 +1,22 @@
 -- fallback -> type command without combo
-vim.keymap.set("n", "<leader>g", ":G ")
+vim.keymap.set('n', '<leader>g', ':G ')
 
--- Git status alternatives
-vim.keymap.set("n", "<leader>gs", ':G<CR>5j')                 -- fugitive interface with status and interative staging. 5j to move to first listed file
-vim.keymap.set("n", "<leader>gvs", ':G<CR><C-w>H5j')              -- vertical git status
+-- fugitive status. 5j to move to first listed file
+vim.keymap.set('n', '<leader>gs', ':G<CR>5j')
+vim.keymap.set('n', '<leader>gvs', ':G<CR><C-w>H5j')
 
 -- basic workflow commands
-vim.keymap.set('n', '<leader>gf', ":G fetch<CR>")               -- fetch
-vim.keymap.set('n', '<leader>gj', ":G pull<CR>")                -- pull
-vim.keymap.set("n", "<leader>ga", ":G add ")                    -- opens git add command ready for parameters
-vim.keymap.set("n", "<leader>gc", ":G commit -m \"\"<left>")    -- opens git commit with the cursor between quotes
-vim.keymap.set('n', '<leader>gC', ":G commit --amend")
-vim.keymap.set("n", "<leader>gk", ":G push ")                    -- no <CR> at the end to prevent accidents
-vim.keymap.set("n", "<leader>guk", ":G push -u origin ")
+vim.keymap.set('n', '<leader>gf', ':G fetch<CR>')
+vim.keymap.set('n', '<leader>gj', ':G pull<CR>')
+vim.keymap.set('n', '<leader>ga', ':G add ')
+vim.keymap.set('n', '<leader>gc', ':G commit -m ""<left>')
+vim.keymap.set('n', '<leader>gC', ':G commit --amend')
+vim.keymap.set('n', '<leader>gk', ':G push ')
+vim.keymap.set('n', '<leader>guk', ':G push -u origin ')
 
 -- "advanced" commands
 vim.keymap.set('n', '<leader>gF', ':G fetch --all<CR>')
-vim.keymap.set("n", "<leader>gR", ":G reset HEAD~")
+vim.keymap.set('n', '<leader>gR', ':G reset HEAD~')
 vim.keymap.set('n', '<leader>gK', ':G push -f ')
 vim.keymap.set('n', '<leader>gJ', ':G pull -f ')
 vim.keymap.set('n', '<leader>gI', ':G rebase -i ')
@@ -29,18 +29,18 @@ vim.keymap.set('n', '<leader>gDj', ':diffget //2<CR>')
 vim.keymap.set('n', '<leader>gDk', ':diffget //3<CR>')
 
 -- info
-vim.keymap.set("n", "<leader>gd", ":G diff<CR>")
-vim.keymap.set("n", "<leader>gl", ":G log<CR>")
-vim.keymap.set("n", "<leader>gvl", ":G log<CR><C-w>H")
-vim.keymap.set("n", "<leader>gbm", ":G blame<CR>")
+vim.keymap.set('n', '<leader>gd', ':G diff<CR>')
+vim.keymap.set('n', '<leader>gl', ':G log<CR>')
+vim.keymap.set('n', '<leader>gvl', ':G log<CR><C-w>H')
+vim.keymap.set('n', '<leader>gbm', ':G blame<CR>')
 
 -- stashing
-vim.keymap.set("n", "<leader>gtk", ":G stash<CR>") -- add
-vim.keymap.set("n", "<leader>gtK", ":G stash --staged<CR>") -- add staged only
-vim.keymap.set("n", "<leader>gtj", ":G stash pop") -- pop
-vim.keymap.set("n", "<leader>gtd", ":G stash drop") -- drop
-vim.keymap.set("n", "<leader>gta", ":G stash apply") -- apply
-vim.keymap.set("n", "<leader>gts", ":G stash show<CR>") -- log
+vim.keymap.set('n', '<leader>gtk', ':G stash<CR>')
+vim.keymap.set('n', '<leader>gtK', ':G stash --staged<CR>')
+vim.keymap.set('n', '<leader>gtj', ':G stash pop')
+vim.keymap.set('n', '<leader>gtd', ':G stash drop')
+vim.keymap.set('n', '<leader>gta', ':G stash apply')
+vim.keymap.set('n', '<leader>gts', ':G stash show<CR>')
 
 -- restore
 vim.keymap.set('n', '<leader>grf', ':G restore ')
@@ -48,12 +48,12 @@ vim.keymap.set('n', '<leader>gra', ':G restore .')
 vim.keymap.set('n', '<leader>grc', ':G restore --cached ')
 
 -- branch
-vim.keymap.set('n', '<leader>gbc', ":G checkout ")              -- Checkout
-vim.keymap.set('n', '<leader>gbn', ":G checkout -b ")           -- Checkout new
-vim.keymap.set('n', '<leader>gm', ":G merge ")                  -- merge - requires argument
-vim.keymap.set('n', '<leader>gbd', ":G branch -d ")             -- delete branch
-vim.keymap.set('n', '<leader>gbD', ":G branch -D ")             -- delete branch even it not fully merged
-vim.keymap.set('n', '<leader>gbkd', ":G push origin --delete ") -- delete branch from remote
+vim.keymap.set('n', '<leader>gbc', ':G checkout ')
+vim.keymap.set('n', '<leader>gbn', ':G checkout -b ')
+vim.keymap.set('n', '<leader>gm', ':G merge ')
+vim.keymap.set('n', '<leader>gbd', ':G branch -d ')
+vim.keymap.set('n', '<leader>gbD', ':G branch -D ')
+vim.keymap.set('n', '<leader>gbkd', ':G push origin --delete ')
 
 -- craziness
-vim.keymap.set('n', '<leader>gusm', ':G submodule update --init --recursive', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gusm', ':G submodule update --init --recursive')
