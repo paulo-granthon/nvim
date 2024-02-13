71cf5202d543742bbf2f80e0ca103fcb6b349e6c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-13 10:27:31 -0300

    chore: remove `tag` in `nvim-surround` plugin in `cfg.lazy`

--- lua/cfg/lazy.lua
+++ lua/cfg/lazy.lua
@@ -111,7 +111,6 @@ return require('lazy').setup({
   -- Quick surrounding with '(', ''', '{', etc
   {
     'kylechui/nvim-surround',
-    tag = '*', -- Use for stability; omit to use `main` branch for the latest features
     config = function()
       require('nvim-surround').setup({
         -- Configuration here, or leave empty to use defaults
