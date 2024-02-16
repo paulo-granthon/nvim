c7d4049f6bc6e1016c783fc44e0840dd2f9b476c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-16 02:05:17 -0300

    style: format code in `plugin.telescope`

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -2,8 +2,14 @@ local telescope_builtin_ok, telescope_builtin_or_err = pcall(require, 'telescope
 if not telescope_builtin_ok then return telescope_builtin_or_err end
 
 local builtin = telescope_builtin_or_err
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({
+    search = vim.fn.input('Grep > '),
+  })
+end)
+
 end)
