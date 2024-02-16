9c574ef486bd777185199aa5ccebb08787171fe0
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-16 02:04:38 -0300

    feat: handle `telescope.builtin` import error in `plugin.telescope`

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,4 +1,7 @@
-local builtin = require('telescope.builtin')
+local telescope_builtin_ok, telescope_builtin_or_err = pcall(require, 'telescope.builtin')
+if not telescope_builtin_ok then return telescope_builtin_or_err end
+
+local builtin = telescope_builtin_or_err
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
