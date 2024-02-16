d4a65f627810d019880731d186de0fa831126ae1
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-16 02:05:45 -0300

    feat: functions to get selected text in `plugin.telescope`

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -12,4 +12,27 @@ vim.keymap.set('n', '<leader>ps', function()
   })
 end)
 
+vim.keymap.set('n', '<leader>pr', function()
+  builtin.grep_string({
+    search = vim.fn.input('Grep > '),
+    use_regex = true,
+  })
+end)
+
+local function get_selection_text_by_line()
+  local _, ls, cs = unpack(vim.fn.getpos('v'))
+  local _, le, ce = unpack(vim.fn.getpos('.'))
+
+  if ls > le or (ls == le and cs > ce) then
+    ls, cs, le, ce = le, ce, ls, cs
+  end
+
+  return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
+end
+
+local function get_selected_text()
+  local selected_text_table = get_selection_text_by_line()
+  return table.concat(selected_text_table, '\n')
+end
+
 end)
