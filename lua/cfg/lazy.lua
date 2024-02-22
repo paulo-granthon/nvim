1331dddd47f7a6fe467e4c40b3ded5ed506916ec
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-21 22:51:26 -0300

    feat: add `ray-x/go.nvim` plugin

--- lua/cfg/lazy.lua
+++ lua/cfg/lazy.lua
@@ -204,4 +204,17 @@ return require('lazy').setup({
     'iamcco/markdown-preview.nvim',
     build = function() fn['mkdp#util#install']() end,
   },
+
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function() require('go').setup() end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 })
