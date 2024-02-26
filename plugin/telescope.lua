52ae1c56638cc45f30bfec4ec0ef944464433f4f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-26 16:41:08 -0300

    feat: allow `hidden` for `find_files` keybinding in `plugin.telescope`

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -3,7 +3,11 @@ if not telescope_builtin_ok then return telescope_builtin_or_err end
 
 local builtin = telescope_builtin_or_err
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+  builtin.find_files({
+    hidden = true,
+  })
+end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 vim.keymap.set('n', '<leader>ps', function()
