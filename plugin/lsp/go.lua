6ba6f3e037b1732fe0d5621200417e81cda1c401
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-09-21 23:32:30 -0300

    feat: add explicit `buildFlags` parameter for `gopls` lsp config

--- plugin/lsp/go.lua
+++ plugin/lsp/go.lua
@@ -1,8 +1,21 @@
+require('lspconfig').gopls.setup({
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      buildFlags = {
+        '-tags=integration',
+      },
+      gofumpt = true,
+    },
+  },
+})
+
 local format_sync_grp = vim.api.nvim_create_augroup('GoImport', {})
+
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
   callback = function()
-    require('go.format').goimport()
+    require('go.format').goimport() --
   end,
   group = format_sync_grp,
 })
