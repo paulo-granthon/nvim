53b851767914b9614ecc54bd1fc51ce5058c2437
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-11-17 19:22:19 -0300

    feat: use `env` for `omnisharp_bin` value in `lsp.cs` config

--- plugin/lsp/cs.lua
+++ plugin/lsp/cs.lua
@@ -1,6 +1,9 @@
 local pid = vim.fn.getpid()
 
-local omnisharp_bin = '/usr/local/bin/omnisharp-roslyn/OmniSharp'
+-- Load env for `omnisharp`
+local env_ok, env_or_err = pcall(require, 'env.omnisharp')
+
+local omnisharp_bin = env_ok and env_or_err.omnisharp_bin or '/usr/lib/omnisharp-roslyn/OmniSharp'
 
 require('lspconfig').omnisharp.setup({
   cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) },
