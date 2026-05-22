05396c40eb60172dd33a410090a644fb8eb134b3
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2026-05-22 15:34:43 -0300

    docs(autocmd): document existing autocmd logic

--- lua/cfg/autocmd.lua
+++ lua/cfg/autocmd.lua
@@ -3,12 +3,14 @@ local silent = true
 local oil_cmd = 'Oil'
 local nrw_cmd = 'Explore'
 
+-- Wrap given command in a function that can be called by an autocmd callback
 local wrap_cmd_in_function = function(cmd)
   local silent_flag = silent and 'silent! ' or ''
   cmd = silent_flag .. cmd
   return function() vim.cmd(cmd) end
 end
 
+-- Add a check to the given function to only execute if no file argument is provided (or if the argument is '.')
 local add_no_file_arg_check_to_function = function(fun_of_choice)
   return function()
     local _, target_arg = pcall(function() return vim.v.argv[3] end)
@@ -16,11 +18,18 @@ local add_no_file_arg_check_to_function = function(fun_of_choice)
   end
 end
 
+-- Get the function to execute on VimEnter, checking for oil first and falling back to netrw if oil is not available
 local get_no_file_arg_function = function()
   local oil_ok, _ = pcall(require, 'oil')
   return add_no_file_arg_check_to_function(wrap_cmd_in_function(oil_ok and oil_cmd or nrw_cmd))
 end
 
+-- ============================================================================
+-- Autocommands
+-- ============================================================================
+
+-- On VimEnter, if no file argument is provided, open the file explorer
+-- (Oil if available, otherwise netrw)
 vim.api.nvim_create_autocmd({ 'VimEnter' }, {
   callback = vim.schedule_wrap(function()
     local vim_enter_no_arg_function = get_no_file_arg_function()
